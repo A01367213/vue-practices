2e200dcc111979244a4af67f5739a03176d4e106
Horacio <destroyer_all@hotmail.es>
2022-03-31 21:43:16 -0600

    vue practice 3-1

--- VueP3/app.js
+++ VueP3/app.js
@@ -4,10 +4,52 @@ const app = Vue.createApp({
       counter: 30,
       name : '',
       confirmedName: '',
+      lastName: '',
+      //fullName: '',
     };
   },
 
+  // Investigate more about watchers
+  watch: {
+    counter(value){
+      if (value > 50){
+        this.counter = 0;
+      }
+    }
+    // name(value) {
+    //   if (value === ''){
+    //     this.fullName = '';
+    //   }
+    //   else{
+    //     this.fullName = value + ' ' + this.lastName;
+    //   }
+    // },
+
+    // lastName(value) {
+    //   if (value === ''){
+    //     this.fullName = '';
+    //   }
+    //   else{
+    //     this.fullName = this.name + ' ' + value;
+    //   }
+    // }
+  },
+
+  // Same as methods, but executed as data property
+  // Just point methods (No ()) 
+  // Used for outputs
+  computed:{
+    fullName(){
+      console.log("Executed again");
+      if (this.name == ''){
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    }
+  },
+
   //Methods can be called with or without ()
+  // Use methods when recalculating (as events)
   methods: {
     confirmInput(){
       this.confirmedName = this.name;
@@ -21,12 +63,24 @@ const app = Vue.createApp({
       this.counter = this.counter - num;
     },
 
-    setName(event, lastName){
-      this.name = event.target.value + ' ' + lastName;
+    setName(event){
+      this.name = event.target.value;
     },
 
     submitForm(){
       alert('Submitted!');
+    },
+
+    resetInput(){
+      this.name = '';
+    },
+
+    outputFullName(){
+      console.log("Executed again");
+      if (this.name == ''){
+        return '';
+      }
+      return this.name + ' ' + 'Lamas';
     }
     
     //Traditional
